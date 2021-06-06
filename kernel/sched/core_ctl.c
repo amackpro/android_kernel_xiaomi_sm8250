bafc95afde7c363b0488c44587bbe1e5d868ced7
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2021-06-06 17:08:43 +0000

    sched: core_ctl: stop userspace from messing it with shitty adjustments
    let kernel to force stop the rom from messing jitter to achieve low jitter
    Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
    
    removed redundant conditional directive and make it look clean.
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -1249,6 +1249,7 @@ static int core_ctl_isolation_dead_cpu(unsigned int cpu)
 
 /* ============================ init code ============================== */
 
+#if 0
 static struct cluster_data *find_cluster_by_first_cpu(unsigned int first_cpu)
 {
 	unsigned int i;
@@ -1355,3 +1356,4 @@ static int __init core_ctl_init(void)
 }
 
 late_initcall(core_ctl_init);
+#endif
