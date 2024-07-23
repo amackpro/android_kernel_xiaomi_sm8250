de4bf95e59658a1b2e43a4618ca03ca8dbb7725c
amackpro <prajwal.kumar008@gmail.com>
2024-07-23 12:11:41 +0530

    drivers: drop KPROFILE

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -509,7 +509,6 @@ static int cgroup_pidlist_show(struct seq_file *s, void *v)
 	return 0;
 }
 
-extern int kp_active_mode(void);
 static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 				     char *buf, size_t nbytes, loff_t off,
 				     bool threadgroup)
@@ -545,17 +544,6 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 		goto out_finish;
 
 	ret = cgroup_attach_task(cgrp, task, threadgroup);
-
-	/* This covers boosting for app launches and app transitions */
-	if (!ret && !threadgroup &&
-		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
-		task_is_zygote(task->parent)) {
-		if (kp_active_mode() == 3 || kp_active_mode() == 0) {
-			devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 500);
-		} else if (kp_active_mode() == 2) {
-			devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 250);
-		}
-	}
 
 out_finish:
 	cgroup_procs_write_finish(task);
