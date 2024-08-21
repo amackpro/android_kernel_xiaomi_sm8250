3f5ed94cc2ea75108f86857857a8313d278c30c9
amackpro <prajwal.kumar008@gmail.com>
2024-08-21 17:04:44 +0530

    fixup ! build error

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -213,7 +213,6 @@ struct irq_data {
  * IRQD_PERF_CRITICAL		- IRQ is performance-critical
  * IRQD_AFFINITY_ON_ACTIVATE	- Affinity is set on activation. Don't call
  *				  irq_chip::irq_set_affinity() when deactivated.
- * IRQD_PERF_CRITICAL		- IRQ is performance-critical
  */
 enum {
 	IRQD_TRIGGER_MASK		= 0xf,
@@ -239,7 +238,6 @@ enum {
 	IRQD_MSI_NOMASK_QUIRK		= (1 << 27),
 	IRQD_PERF_CRITICAL		= (1 << 28),
 	IRQD_AFFINITY_ON_ACTIVATE	= (1 << 29),
-	IRQD_PERF_CRITICAL		= (1 << 30),
 };
 
 #define __irqd_to_state(d) ACCESS_PRIVATE((d)->common, state_use_accessors)
